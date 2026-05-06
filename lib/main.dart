fe6b40d301623dfbe8c9b58b0451b9873572f405
MohamedHamada <mohamedweka446@gmail.com>
2026-05-06 20:46:18 +0300

    Bengo app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BengoApp());
+}
+
+class BengoApp extends StatelessWidget {
+  const BengoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
